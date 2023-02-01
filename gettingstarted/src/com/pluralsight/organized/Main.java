cc1a00e31d8dff8ae1c1eb5f5a3dd8dfc8e206cf
AlejandriaRamai <vyasadev931@gmail.com>
2023-02-01 09:27:49 +0100

    refactoring

---
+++ gettingstarted/src/com/pluralsight/organized/Main.java
@@ -0,0 +1,17 @@
+package com.pluralsight.organized;
+
+public class Main {
+
+    /*
+    This is the first comment line
+    This is the second comment line
+    // this is a line comment
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("First line from app"); // slash comment
+     //   System.out.println("Second line from app"); //
+        System.out.println(/*"Third line from app"*/"This is different text");
+    }
+}
---
+++ gettingstarted/src/com/pluralsight/organized/Organization.java
@@ -0,0 +1,8 @@
+package com.pluralsight.organized;
+
+public class Organization {
+
+    public static void main(String[] args) {
+        System.out.println("We got organized");
+    }
+}
