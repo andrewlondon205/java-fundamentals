fe71145c6a36c179320bdd5a528b324891588896
Ramai Alejandria <vyasadev931@gmail.com>
2023-02-08 15:51:14 +0100

    interactive calculations

--- gettingstarted/src/com/pluralsight/organized/calcengine/Engine.java
+++ gettingstarted/src/com/pluralsight/organized/calcengine/Engine.java
@@ -1,6 +1,6 @@
-package com.pluralsight.organized.calengine;
+package com.pluralsight.organized.calcengine;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Engine {
 
@@ -21,12 +21,33 @@ public class Engine {
             for (double value : results) {
                 System.out.println(value);
             }
-        } else if(args.length == 3) {
+
+        } else if (args.length == 1 && args[0].equals("interactive")){
+            executeInteractively();
+        }
+        else if(args.length == 3) {
             handleCommandLine(args);
         }
         else {
             System.out.println("Please provide an operation and 2 numeric values");
         }
+    }
+
+    public static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String [] parts = userInput.split(" ");
+        performOperation(parts);
+        
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode,leftVal,rightVal);
+        System.out.println(result);
     }
 
     private static void handleCommandLine(String[] args) {
@@ -59,4 +80,22 @@ public class Engine {
         }
         return result;
     }
+
+    public static char opCodeFromString(String operationName) {
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    public static double valueFromWord(String word) {
+        String [] numberWords = { "zero","one","two","three","four","five","six","seven","eight","nine"};
+        double value = 0d;
+        for(int index = 0; index < numberWords.length; index++) {
+            if(word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
+    }
+
 }
